14d0878401a1b0c84da034bf2ae639a4068053ce
Yusuke KUOKA <ykuoka@gmail.com>
2018-04-11 16:55:20 +0900

    Prompt before updating
    Ref #1112

--- cmd/update.go
+++ cmd/update.go
@@ -3,8 +3,11 @@ package cmd
 import (
 	"fmt"
 
+	"bufio"
 	"github.com/kubernetes-incubator/kube-aws/core/root"
 	"github.com/spf13/cobra"
+	"os"
+	"strings"
 )
 
 var (
@@ -19,6 +22,7 @@ var (
 	updateOpts = struct {
 		awsDebug, prettyPrint, skipWait bool
 		s3URI                           string
+		force                           bool
 	}{}
 )
 
@@ -27,9 +31,15 @@ func init() {
 	cmdUpdate.Flags().BoolVar(&updateOpts.awsDebug, "aws-debug", false, "Log debug information from aws-sdk-go library")
 	cmdUpdate.Flags().BoolVar(&updateOpts.prettyPrint, "pretty-print", false, "Pretty print the resulting CloudFormation")
 	cmdUpdate.Flags().BoolVar(&updateOpts.skipWait, "skip-wait", false, "Don't wait the resources finish")
+	cmdUpdate.Flags().BoolVar(&updateOpts.force, "force", false, "Don't ask for confirmation")
 }
 
 func runCmdUpdate(cmd *cobra.Command, args []string) error {
+	if !updateOpts.force && !updateConfirmation() {
+		fmt.Println("Operation cancelled")
+		return nil
+	}
+
 	opts := root.NewOptions(updateOpts.prettyPrint, updateOpts.skipWait)
 
 	cluster, err := root.ClusterFromFile(configPath, opts, updateOpts.awsDebug)
@@ -61,4 +71,13 @@ func runCmdUpdate(cmd *cobra.Command, args []string) error {
 	fmt.Printf(successMsg, info.String())
 
 	return nil
+}
+
+func updateConfirmation() bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("This operation will update the cluster. Are you sure? [y,n]: ")
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSuffix(strings.ToLower(text), "\n")
+
+	return text == "y" || text == "yes"
 }
