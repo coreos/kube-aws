b41785062a1dd58d5e403baa0b8170bfbadc9c02
David McCormick <davidemccormick@gmail.com>
2020-02-18 21:11:58 +0000

    Migrate to flatcar linux from coreos

--- flatcar/amiregistry/amiregistry.go
+++ flatcar/amiregistry/amiregistry.go
@@ -3,31 +3,35 @@ package amiregistry
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/pkg/errors"
 )
 
 func GetAMI(region, channel string) (string, error) {
 
-	regions, err := GetAMIData(channel)
+	amis, err := GetAMIData(channel)
 
 	if err != nil {
 		return "", errors.Wrapf(err, "uanble to fetch AMI for channel \"%s\": %v", channel, err)
 	}
 
-	amis, ok := regions[region]
-	if !ok {
-		return "", errors.Errorf("could not find region \"%s\" for channel \"%s\"", region, channel)
-	}
-
-	if ami, ok := amis["hvm"]; ok {
-		return ami, nil
+	for _, v := range amis {
+		if reg, ok := v["name"]; ok {
+			if reg == region {
+				if hvm, ok := v["hvm"]; ok {
+					return hvm, nil
+				} else {
+					return "", fmt.Errorf("region %s does not have a flatcar hvm ami entry", region)
+				}
+			}
+		}
 	}
 
 	return "", errors.Errorf("could not find \"hvm\" image for region \"%s\" and channel \"%s\"", region, channel)
 }
 
-func GetAMIData(channel string) (map[string]map[string]string, error) {
-	url := fmt.Sprintf("https://coreos.com/dist/aws/aws-%s.json", channel)
+func GetAMIData(channel string) ([]map[string]string, error) {
+	url := fmt.Sprintf("https://%s.release.flatcar-linux.net/amd64-usr/current/flatcar_production_ami_all.json", channel)
 	r, err := newHttp().Get(url)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to get AMI data from url \"%s\": %v", channel, err)
@@ -37,7 +41,7 @@ func GetAMIData(channel string) (map[string]map[string]string, error) {
 		return nil, errors.Wrapf(err, "failed to get AMI data from url \"%s\": invalid status code: %d", url, r.StatusCode)
 	}
 
-	output := map[string]map[string]string{}
+	output := map[string][]map[string]string{}
 
 	err = json.NewDecoder(r.Body).Decode(&output)
 	if err != nil {
@@ -45,5 +49,5 @@ func GetAMIData(channel string) (map[string]map[string]string, error) {
 	}
 	r.Body.Close()
 
-	return output, nil
+	return output["amis"], nil
 }
